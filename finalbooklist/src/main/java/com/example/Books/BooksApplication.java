2840c9fc1ce5c9a07f7623058c2f954a8031b14b
Shyam S <mshyams930@gmail.com>
2024-10-10 02:19:06 +0530

    Add files via upload

---
+++ finalbooklist/src/main/java/com/example/Books/BooksApplication.java
@@ -0,0 +1,17 @@
+package com.example.Books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableCaching
+@EnableDiscoveryClient
+@SpringBootApplication
+public class BooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksApplication.class, args);
+	}
+
+}
